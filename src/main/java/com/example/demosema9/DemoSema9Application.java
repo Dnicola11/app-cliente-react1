6c1051680d389bc113478010d4ecc705b94ade0f
Dnicola11 <135287080+Dnicola11@users.noreply.github.com>
2023-07-21 03:06:27 +0000

    commit 1

---
+++ src/main/java/com/example/demosema9/DemoSema9Application.java
@@ -0,0 +1,13 @@
+package com.example.demosema9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSema9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSema9Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demosema9/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.demosema9;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> { 
+
+}
+
